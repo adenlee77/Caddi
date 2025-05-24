5dd5671c9d4d665fbe05b5318908662200c36741
adenlee77 <adenleeuw@gmail.com>
2025-05-24 15:34:33 -0400

    added more angles

---
+++ main.py
@@ -0,0 +1,76 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+from pose_utils import (
+    get_left_knee_angle,
+    get_right_elbow_angle,
+    get_left_elbow_angle,
+    get_right_knee_angle,
+    get_spine_angle
+)
+
+cap = cv2.VideoCapture(1)
+
+# Set up mediapipe instance
+with mp_pose.Pose() as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        # Change colour to RGB, make a detection in RGB, then switch back to BGR
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = pose.process(image)
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Get exact landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+
+            left_knee_angle, left_knee_point = get_left_knee_angle(landmarks)
+            left_elbow_angle, left_elbow_point = get_left_elbow_angle(landmarks)
+            right_knee_angle, right_knee_point = get_right_knee_angle(landmarks)
+            right_elbow_angle, right_elbow_point = get_right_elbow_angle(landmarks)
+            spine_angle, shoulder_center = get_spine_angle(landmarks)
+
+            # Get dimensions from the frame
+            frame_height, frame_width, _ = image.shape
+            
+            # Display angle on the joint
+            cv2.putText(image, f"{int(left_knee_angle)}",
+                        tuple(np.multiply(left_knee_point, [frame_width, frame_height]).astype(int)),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (250, 60, 5), 2)
+            
+            cv2.putText(image, f"{int(right_knee_angle)}",
+                        tuple(np.multiply(right_knee_point, [frame_width, frame_height]).astype(int)),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (250, 60, 5), 2)
+
+            cv2.putText(image, f"{int(left_elbow_angle)}",
+                        tuple(np.multiply(left_elbow_point, [frame_width, frame_height]).astype(int)),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (250, 60, 5), 2)
+            
+            cv2.putText(image, f"{int(right_elbow_angle)}",
+                        tuple(np.multiply(right_elbow_point, [frame_width, frame_height]).astype(int)),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (250, 60, 5), 2)
+
+            cv2.putText(image, f"{int(spine_angle)}",
+                tuple(np.multiply(shoulder_center, [frame_width, frame_height]).astype(int)),
+                cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (250, 60, 5), 2)
+
+        except:
+            print("Fail")
+            pass
+
+        # Draw detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        cv2.imshow('Webcam Feed', image)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows
+
