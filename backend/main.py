89c9be241574b831cfdf0c0f312fa4aa22a8c804
adenlee77 <adenleeuw@gmail.com>
2025-08-11 17:31:15 -0400

    getting ready for deploy

--- backend/main.py
+++ backend/main.py
@@ -116,4 +116,4 @@ def results():
     return render_template('feedback.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
